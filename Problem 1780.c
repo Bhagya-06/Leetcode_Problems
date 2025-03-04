f50d8513eed2a235d78a27022d34aa95f63441fc
Bhagya B <114055494+Bhagya-06@users.noreply.github.com>
2025-03-04 22:50:07 +0530

    Create Problem 1780.c

---
+++ Problem 1780.c
@@ -0,0 +1,9 @@
+// Problem 1780: Check if Number is a Sum of Powers of Three
+
+bool checkPowersOfThree(int n) {
+    while(n>0){
+        if (n % 3 == 2) return false;
+        n/=3;
+    }
+    return true;
+}
